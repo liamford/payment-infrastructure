6b4440a73421a64a1d1b3ccb94145861852f74cd
liamford <liam.ford.au@gmail.com>
2024-12-27 23:14:40 +1100

    Added terraform for K8s resources

--- main.tf
+++ main.tf
@@ -105,3 +105,128 @@ module "irsa-ebs-csi" {
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
+
+resource "kubernetes_namespace" "frontdoor-ns" {
+  metadata {
+    name = "frontdoor-ns"
+  }
+}
+
+resource "aws_iam_policy" "s3_read_policy" {
+  name        = "S3ReadPolicy"
+  description = "Policy to allow read access to S3"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = ["s3:GetObject"],
+        Effect   = "Allow",
+        Resource = "arn:aws:s3:::your-bucket-name/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "dynamodb_full_access_policy" {
+  name        = "DynamoDBFullAccessPolicy"
+  description = "Policy to allow full access to DynamoDB"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = ["dynamodb:*"],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "eks_pod_role" {
+  name = "eks_pod_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_read_policy_attachment" {
+  role       = aws_iam_role.eks_pod_role.name
+  policy_arn = aws_iam_policy.s3_read_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_full_access_policy_attachment" {
+  role       = aws_iam_role.eks_pod_role.name
+  policy_arn = aws_iam_policy.dynamodb_full_access_policy.arn
+}
+
+
+
+resource "kubernetes_service_account" "frontdoor-service-account" {
+  metadata {
+    name      = "frontdoor-service-account"
+    namespace = kubernetes_namespace.frontdoor-ns.metadata[0].name
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.eks_pod_role.arn
+    }
+  }
+}
+
+
+resource "aws_apigatewayv2_api" "main" {
+  name          = "main"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "prod" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  name        = "prod"
+  auto_deploy = true
+}
+
+resource "aws_security_group" "vpc_link" {
+  name   = "vpc-link"
+  vpc_id = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_apigatewayv2_vpc_link" "eks" {
+  name               = "eks"
+  security_group_ids = [aws_security_group.vpc_link.id]
+  subnet_ids = [
+    module.vpc.private_subnets[0],
+    module.vpc.private_subnets[1]
+
+  ]
+}
+
+resource "aws_apigatewayv2_integration" "eks" {
+  api_id             = aws_apigatewayv2_api.main.id
+  integration_type   = "HTTP_PROXY"
+  integration_method = "ANY"
+  integration_uri    = module.eks.cluster_endpoint
+  connection_type    = "VPC_LINK"
+  connection_id      = aws_apigatewayv2_vpc_link.eks.id
+}
+
+resource "aws_apigatewayv2_route" "eks" {
+  api_id    = aws_apigatewayv2_api.main.id
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.eks.id}"
+}
