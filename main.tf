2c2c65afb7a19a9ba8ce719a6ad500b0837d98c8
liamford <liam.ford.au@gmail.com>
2024-12-31 16:15:00 +1100

    updates security group

--- main.tf
+++ main.tf
@@ -221,6 +221,13 @@ resource "aws_security_group" "vpc_link" {
   name   = "vpc-link"
   vpc_id = module.vpc.vpc_id
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -243,7 +250,7 @@ resource "aws_apigatewayv2_integration" "eks" {
   api_id = aws_apigatewayv2_api.main.id
   integration_uri = "arn:aws:elasticloadbalancing:ap-southeast-2:765874878578:listener/net/aef3c770d2fa84e4f9b192f8356681d9/8d12ee2d55f2d79d/6688639805ad8453"
   integration_type   = "HTTP_PROXY"
-  integration_method = "ANY"
+  integration_method = "GET"
   connection_type    = "VPC_LINK"
   connection_id      = aws_apigatewayv2_vpc_link.eks.id
   payload_format_version = "1.0"
