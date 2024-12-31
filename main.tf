d36b8f9456cca787c362fe3a4edcd673c0f38e13
liamford <liam.ford.au@gmail.com>
2024-12-31 16:19:13 +1100

    kossher

--- main.tf
+++ main.tf
@@ -222,10 +222,10 @@ resource "aws_security_group" "vpc_link" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
